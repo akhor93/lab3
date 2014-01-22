2d8acf8b19b31ceed1a119bbbbfd92c9270a2a3c
Andrew Khor <akhor93@gmail.com>
2014-01-22 15:00:06 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,40 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Why did you click me?! That hurts!");
+		$(".jumbotron p").toggleClass("active");
 	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+	$("#submitBtn").click(function(e) {
+		var projectId = $("#project").val();
+		var width = $("#width").val();
+		var description = $("#description").val();
+		$(projectId).animate({
+			width: width
+		});
+		$(projectId + " .project-description").text(description);
+	});
+}
+
+function projectClick(e) {
+	e.preventDefault();
+
+	var projectTitle = $(this).find("p").text();
+  var jumbotronHeader = $(".jumbotron h1");
+  jumbotronHeader.text(projectTitle);
+
+  var containingProject = $(this).closest(".project");
+  var description = $(containingProject).find(".project-description");
+
+  if(description.length == 0) {
+  	$(containingProject).append("<div class='project-description'><p>Description of the project</p></div>");
+  } else {
+  	// description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");  	$(containingProject).children(".project-description").fadeOut();
+  }
+  
 }
